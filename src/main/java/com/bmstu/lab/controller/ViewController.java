49a3c3acb34ff191a7c2acfb4d44b6b0d50fafc7
Exist <existqq@gmail.com>
2025-09-10 14:15:03 +0300

    refactor: improve minio integration

--- src/main/java/com/bmstu/lab/controller/ViewController.java
+++ src/main/java/com/bmstu/lab/controller/ViewController.java
@@ -3,7 +3,7 @@ package com.bmstu.lab.controller;
 import com.bmstu.lab.model.Service;
 import com.bmstu.lab.repository.service.ServiceRepository;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequiredArgsConstructor
 public class ViewController {
 
   private final ServiceRepository serviceRepository;
+
+  private final String MINIO_BASE_URL;
+
+  public ViewController(ServiceRepository serviceRepository, @Value("${minio.base-url}")String MINIO_BASE_URL) {
+    this.serviceRepository = serviceRepository;
+    this.MINIO_BASE_URL = MINIO_BASE_URL;
+  }
 
   @GetMapping("/services")
   public String servicesPage(@RequestParam(required = false) String query, Model model) {
@@ -28,6 +34,8 @@ public class ViewController {
 
     model.addAttribute("services", services);
     model.addAttribute("query", query);
+    model.addAttribute("cart", serviceRepository.findServicesByCart(1L).size());
+    model.addAttribute("baseUrl", MINIO_BASE_URL);
 
     return "main";
   }
